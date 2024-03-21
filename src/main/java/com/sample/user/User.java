cd45a9ec300a2b559c4ac68c289bd571a7751f8e
MinjuJi <hinote444@gmail.com>
2024-03-21 10:03:32 +0900

    feat: 회원가입 시 유저권한 지정 기능 구현

--- src/main/java/com/sample/user/User.java
+++ src/main/java/com/sample/user/User.java
@@ -5,6 +5,8 @@ import com.sample.common.BaseDateTimeEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -44,6 +46,10 @@ import lombok.Setter;
  *  		- default 기본키 생성 방식이다.
  *  		- 데이터베이스에 따라 적절한 기본키를 자동으로 생성한다.
  * 
+ * @Enumerated
+ * 	- 이 어노테이션은 상수 데이터의 주요한 값 중에서 어떤 값을 엔티티에서 사용할 것인지 지정하는 어노테이션이다.
+ *  - EnumType.STRING - 상수데이터의 이름을 사용한다.
+ *  - EnumType.ORDINAL - 상수데이터의 순서(0 ,1 , ...)을 사용한다.
  */
 @Entity
 @Table(name="board_users")
@@ -75,5 +81,8 @@ public class User extends BaseDateTimeEntity{
 	
 	@Column(nullable=false)
 	private String tel;
+	
+	@Enumerated(EnumType.STRING)
+	private UserRole userRole;
 	
 }
